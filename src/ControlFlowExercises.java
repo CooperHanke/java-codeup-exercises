8dcb28d62843c73d18bae99a60d7fea7628a8179
Cooper Hanke <hanke31314@gmail.com>
2018-02-27 13:58:28 -0600

    first refactored loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,7 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 2;
-        System.out.println(i);
-        do {
-            i = (int) Math.pow(i, 2);
+        for (int i = 100; i >= -10; i -= 5) {
             System.out.println(i);
-        } while (i < 65000);
+        }
     }
 }
