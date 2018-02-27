ac2b8f7f8e4876f3920854b8800e4d36ae82244a
coopmostcurious <hanke31314@gmail.com>
2018-02-27 17:49:17 -0600

    almost forgot to add the Bob file. derp

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Bob {
+    public static void main(String[] args) {
+        System.out.println("What up, cuz? I'm Bob. Wut it dew?");
+        Scanner scan = new Scanner(System.in);
+        String response = scan.nextLine();
+
+        if (response.contains("?")) {
+            System.out.println("Sure");
+        } else if (response.equals(response.toUpperCase()) && !response.isEmpty()) {
+            System.out.println("Whoa, chill out!");
+        } else if (response.isEmpty()) {
+            System.out.println("Fine, be that way!");
+        } else System.out.println("Whatever");
+    }
 }
