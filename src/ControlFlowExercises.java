8d4df8f4091fee4b7472de080416092355685f9a
Cooper Hanke <hanke31314@gmail.com>
2018-02-27 13:59:42 -0600

    2nd refactoring done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,6 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 100; i >= -10; i -= 5) {
+        for (int i = 2; i <= 1000000; i = (int) Math.pow(i, 2)) {
             System.out.println(i);
         }
     }
