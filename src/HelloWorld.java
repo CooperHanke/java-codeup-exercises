1853ef4d2e672be7b68062d300ea2908dad2beea
Cooper Hanke <hanke31314@gmail.com>
2018-02-27 00:15:19 -0600

    finished with second exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,23 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World!!!\n");
         System.out.print("Back in the saddle with Java again!");
+        String myString = "My favorite number is: ";
+        int myFavoriteNumber = 7;
+        System.out.print(myString + myFavoriteNumber);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.print(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.print(x + " " + y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y =- x;
+        System.out.print(x + " " + y);
     }
 }
