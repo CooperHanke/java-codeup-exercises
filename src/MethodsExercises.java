8adc1106326aa41a0346d43c2c1a62dcca232c23
coopmostcurious <hanke31314@gmail.com>
2018-02-28 10:15:06 -0600

    finished with initial exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,11 +1,31 @@
 public class MethodsExercises {
     public static void main(String[] args) {
-
-        double num1 = 3;
-        double num2 = 4;
-        System.out.println(addition(num1, num2));
+        calculateMethods();
     }
     private static double addition (double x, double y) {
         return x + y;
+    }
+    private static double subtraction (double x, double y) {
+        return y - x;
+    }
+    private static double multiply (double x, double y) {
+        return x * y;
+    }
+    private static double divide (double x, double y) {
+        return y / x;
+    }
+    private static int remainder (double x, double y) {
+        return (int)(y % x);
+    }
+    private static void calculateMethods() {
+        double num1 = 3;
+        double num2 = 7;
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+        System.out.println(addition(num1, num2));
+        System.out.println(subtraction(num1, num2));
+        System.out.println(multiply(num1, num2));
+        System.out.println(divide(num1, num2));
+        System.out.println("Expected result: 1\nActual Result: " + remainder(num1, num2));
     }
 }
