dbe3f6afacf906623d03b659516c1f18cbf46d5d
coopmostcurious <hanke31314@gmail.com>
2018-02-28 10:05:19 -0600

    finished with the first method, working on the other ones in the first part of the exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,11 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+        double num1 = 3;
+        double num2 = 4;
+        System.out.println(addition(num1, num2));
+    }
+    private static double addition (double x, double y) {
+        return x + y;
+    }
+}
