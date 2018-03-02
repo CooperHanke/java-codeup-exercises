674dc01cde1965e64a7a31464a8136e68f2e9a01
coopmostcurious <hanke31314@gmail.com>
2018-03-02 15:58:45 -0600

    finishing touches on circles part of oop exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -3,7 +3,17 @@ package shapes;
 import util.Input;
 
 public class CircleApp {
-    public static void main(String[] args) {
+    private static int circleCount;
+
+    private static int getCircleCount() {
+        return circleCount;
+    }
+
+    private static void increaseCircleCount() {
+        circleCount++;
+    }
+
+    private static void makeCircle() {
         System.out.println("What is the radius of the circle? ");
         Input input = new Input();
         double userRadius = input.getDouble();
@@ -11,5 +21,17 @@ public class CircleApp {
         System.out.println("The radius was " + userRadius);
         System.out.println("Area: " + userCircle.getArea());
         System.out.println("Circumference: " + userCircle.getCircumference());
+        increaseCircleCount();
+        System.out.println("Do you want to continue? ");
+        boolean toContinue = input.yesNo();
+        if (toContinue) {
+            makeCircle();
+        } else {
+            System.out.println("As you wish, you had made " + getCircleCount() + "!");
+        }
     }
+
+    public static void main(String[] args) {
+        makeCircle();
+        }
 }
