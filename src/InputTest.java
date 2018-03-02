932570e599d134cc6dfc4a2e78916a3abdabc846
coopmostcurious <hanke31314@gmail.com>
2018-03-02 15:05:41 -0600

    finished parts 1 through 3 of exercise; init commit

---
+++ src/InputTest.java
@@ -0,0 +1,35 @@
+import util.Input;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input test = new Input();
+        System.out.print("Please give us a string: ");
+        String string = test.getString();
+        System.out.println(string);
+        System.out.print("Do you want to continue (test)? ");
+        Boolean bool = test.yesNo();
+        System.out.println(bool);
+        System.out.print("What two numbers would you like to see if a number is between? ");
+        int num1 = test.getInt();
+        int num2 = test.getInt();
+        System.out.println("Here is the range: min is " + num1 + " while max is " + num2);
+        System.out.println("Now, let's check it with test.getInt(" + num1 + ", " + num2 + ")");
+        System.out.print("Please give a number between these, or else I'll keep asking: ");
+        int numberToCheck = test.getInt(num1, num2);
+        System.out.println("Great, " + numberToCheck + " is between " + num1 + " and " + num2 + "!");
+
+        System.out.print("What two numbers would you like to see if a number is between? ");
+        double num3 = test.getDouble();
+        double num4 = test.getDouble();
+        System.out.println("Here is the range: min is " + num3 + " while max is " + num4);
+        System.out.println("Now, let's check it with test.getInt(" + num3 + ", " + num4 + ")");
+        System.out.print("Please give a number between these, or else I'll keep asking: ");
+        double anotherNumberToCheck = test.getDouble(num3, num4);
+        System.out.println("Great, " + anotherNumberToCheck + "is between " + num3 + " and " + num4 + "!");
+
+        System.out.println("Never to leave any of our methods useless and idle, give me one int, and then one floating point number: ");
+        int num5 = test.getInt();
+        double num6 = test.getDouble();
+        System.out.println("And here, you are, one int of " + num5 + ", and then one double of " + num6);
+    }
+}
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Person {
+    public String name;
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void sayHello() {
+        System.out.println("Hello, I am " + this.name + "!!");
+    }
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
 }
