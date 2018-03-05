bdab3c68165dadda77c3431bd2cf294121cd9e51
Cooper Hanke <hanke31314@gmail.com>
2018-03-05 09:21:12 -0600

    added files before merging master to get utils

--- src/movies/MovieApplication.java
+++ src/movies/MovieApplication.java
@@ -1,4 +1,14 @@
 package movies;
 
+import
+
 public class MovieApplication {
+    public static void main(String[] args) {
+
+        System.out.print("What would you like to do? >");
+        getChoice();
+    }
+    private static void getChoice() {
+
+    }
 }
