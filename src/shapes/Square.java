a986dfc1a65bbde4850c5366d43ef78472d11661
Cooper Hanke <hanke31314@gmail.com>
2018-03-07 11:54:53 -0600

    changed square to be a type of quad

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,29 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral {
+    private double myside;
     public Square(double side) {
         super(side, side);
+        myside = side;
+    }
+
+    @Override
+    void setLength() {
+        this.length = myside;
+    }
+
+    @Override
+    void setWidth() {
+        this.width = myside;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return myside * 4;
+    }
+
+    @Override
+    public double getArea() {
+        return myside * myside;
     }
 }
