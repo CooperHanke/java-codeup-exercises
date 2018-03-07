d0b2ad0abd5beffd79e801918eb9ca2afe6a6369
Cooper Hanke <hanke31314@gmail.com>
2018-03-07 10:20:21 -0600

    done with exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,10 +1,24 @@
 package shapes;
 
-public class ShapesTest {
+public abstract class ShapesTest implements Measurable{
+    Measurable myShape = new Measurable() {
+        @Override
+        public double getArea() {
+            return myShape.getArea();
+        }
+        public double getPerimeter() {
+            return myShape.getPerimeter();
+        }
+    };
+
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("box1\narea: " + box1.getArea() + " " + "length: " + box1.getPerimeter());
-        Square box2 = new Square(5);
-        System.out.println("box2\narea: " + box2.getArea() + " " + "length: " + box2.getPerimeter());
+        Square square = new Square(10);
+        System.out.println("Square:");
+        System.out.println("Area: " + square.getArea());
+        System.out.println("Perimeter: " + square.getPerimeter());
+        Rectangle rect = new Rectangle(10,5);
+        System.out.println("Rectangle:");
+        System.out.println("Area: " + rect.getArea());
+        System.out.println("Perimeter: " + rect.getPerimeter());
     }
 }
