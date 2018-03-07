930e18948af10e74e684f6b9b188d1321e62b96f
Cooper Hanke <hanke31314@gmail.com>
2018-03-07 11:02:55 -0600

    created shapes in test with measurable objects

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,11 +5,11 @@ public abstract class ShapesTest implements Measurable {
     public static void main(String[] args) {
 //        Measurable myShape = new Measurable();\
         // Cannot be instantiated ^ Not valid code above!!
-        Square square = new Square(10);
+        Measurable square = new Square(10);
         System.out.println("Square:");
         System.out.println("Area: " + square.getArea());
         System.out.println("Perimeter: " + square.getPerimeter());
-        Rectangle rect = new Rectangle(10,5);
+        Measurable rect = new Rectangle(10,5);
         System.out.println("Rectangle:");
         System.out.println("Area: " + rect.getArea());
         System.out.println("Perimeter: " + rect.getPerimeter());
